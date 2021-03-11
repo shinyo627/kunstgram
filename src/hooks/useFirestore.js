c2856f1a588681349b5ab55008fa8e54c8b7acfa
shinyo627 <shinyo627@gmail.com>
2021-03-11 08:34:17 +0100

    Typo fixed in useFireStore.js

--- src/hooks/useFirestore.js
+++ src/hooks/useFirestore.js
@@ -14,7 +14,7 @@ const useFireStore = (collection) => {
         const documents = [];
 
         // Iterating through all documents inside 'image' collection
-        // Setting per image documents ob with all the fields and unique id
+        // Setting per image documents obj with all the fields and unique id
         snapShot.forEach((doc) => {
           documents.push({ ...doc.data(), id: doc.id });
         });
